0d19ff425226ce6a8c8fef3186fa7319440baefd
Mustafa <79022605+mustafafiliz@users.noreply.github.com>
2024-11-04 17:42:31 +0300

    fix: viteconfig, notfound

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -2,6 +2,6 @@
   <div class="h-dvh w-full flex flex-col justify-center text-center font-bold text-2xl">
     404 - Aradığınız Sayfa Bulunamadı
     <br />
-    <router-link to="/" class="text-blue-500">Anasayfa</router-link>
+    <router-link to="/dashboard/home" class="text-blue-500">Anasayfa</router-link>
   </div>
 </template>
