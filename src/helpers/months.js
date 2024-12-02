c85f09aeeab9c604ec1c94ddfd0d5b898f614ef1
Mustafa <79022605+mustafafiliz@users.noreply.github.com>
2024-12-02 12:13:23 +0300

    fix: payrolls

---
+++ src/helpers/months.js
@@ -0,0 +1,50 @@
+export const months = [
+  {
+    month: 1,
+    label: 'Ocak'
+  },
+  {
+    month: 2,
+    label: 'Şubat'
+  },
+  {
+    month: 3,
+    label: 'Mart'
+  },
+  {
+    month: 4,
+    label: 'Nisan'
+  },
+  {
+    month: 5,
+    label: 'Mayıs'
+  },
+  {
+    month: 6,
+    label: 'Haziran'
+  },
+  {
+    month: 7,
+    label: 'Temmuz'
+  },
+  {
+    month: 8,
+    label: 'Ağustos'
+  },
+  {
+    month: 9,
+    label: 'Eylül'
+  },
+  {
+    month: 10,
+    label: 'Ekim'
+  },
+  {
+    month: 11,
+    label: 'Kasım'
+  },
+  {
+    month: 12,
+    label: 'Aralık'
+  }
+]
--- src/pages/Dashboard/Payrolls.vue
+++ src/pages/Dashboard/Payrolls.vue
@@ -1,29 +1,52 @@
 <script setup>
 import BottomNavigation from '@/components/BottomNavigation.vue'
 import { useAxios } from '@/plugins/axios'
-import { ref, onMounted } from 'vue'
+import { useMeStore } from '@/stores/me'
+import { ref, onMounted, computed, watch } from 'vue'
 
+const meStore = useMeStore()
+const startYear = new Date(meStore.user.work_start_date).getFullYear()
+const endYear = ref(new Date().getFullYear())
+const showPdfModal = ref(false)
+const pdfUrl = ref('')
+const yearOptions = computed(() => {
+  const options = []
+  for (let year = startYear; year <= endYear.value; year++) {
+    options.push({
+      value: year,
+      label: year.toString()
+    })
+  }
+  return options
+})
+const selectedYear = ref(endYear.value)
 const payrolls = ref([])
 const { axios } = useAxios()
-const year = ref(new Date().getFullYear())
 
 const getPayrolls = async () => {
   try {
-    const { data } = await axios.get(`/payrolls/${year.value}`)
+    const { data } = await axios.get(`/payrolls/${selectedYear.value}`)
 
-    payrolls.value = data.data
+    payrolls.value = data
   } catch (error) {
     return error
   }
 }
 
-const getPayrollDetail = async (id) => {
+const getPayrollDetail = async (payrolId) => {
   try {
-    const { data } = await axios.get(`/payrolls/detail/${id}`)
+    const { data } = await axios.get(`/payrolls/detail/${payrolId}`)
+
+    pdfUrl.value = data.url
+    showPdfModal.value = true
   } catch (error) {
     return error
   }
 }
+
+watch(selectedYear, () => {
+  getPayrolls()
+})
 
 onMounted(() => {
   getPayrolls()
@@ -55,10 +78,61 @@ onMounted(() => {
       <div class="absolute lg:top-5 top-7 left-1/2 -translate-x-1/2 font-semibold">Bordrolarım</div>
 
       <div class="grid grid-cols-1 gap-4">
-        <template v-for="item in payrolls"> </template>
+        <div
+          class="relative flex items-center justify-between bg-white rounded-2xl py-3 px-4 font-medium"
+        >
+          <select v-model="selectedYear" class="w-full h-full outline-none">
+            <option v-for="option in yearOptions" :key="option.value" :value="option.value">
+              {{ option.label }}
+            </option>
+          </select>
+        </div>
+        <div class="overflow-x-auto">
+          <table class="w-full bg-white rounded-lg overflow-hidden shadow">
+            <thead>
+              <tr class="bg-gray-50">
+                <th class="px-6 py-2 text-left text-sm font-semibold text-gray-700">Yıl</th>
+                <th class="px-6 py-2 text-left text-sm font-semibold text-gray-700">Ay</th>
+                <th class="px-6 py-2 text-right text-sm font-semibold text-gray-700">İşlem</th>
+              </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-200">
+              <tr v-for="item in payrolls" :key="item._id" class="hover:bg-gray-50">
+                <td class="px-6 py-3 text-sm text-gray-700">{{ item.year }}</td>
+                <td class="px-6 py-3 text-sm text-gray-700">
+                  {{
+                    new Date(item.year, item.month - 1).toLocaleString('tr-TR', { month: 'long' })
+                  }}
+                </td>
+                <td class="px-6 py-3 text-right">
+                  <button
+                    @click="getPayrollDetail(item._id)"
+                    class="underline py-2 rounded-full text-xs font-semibold transition-colors"
+                  >
+                    Bordro Görüntüle
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
 
     <BottomNavigation />
+  </div>
+  <div
+    v-if="showPdfModal"
+    class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+  >
+    <div class="bg-white p-4 rounded-lg w-11/12 h-5/6 relative">
+      <button
+        @click="showPdfModal = false"
+        class="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
+      >
+        ✕
+      </button>
+      <iframe :src="pdfUrl" class="w-full h-full" frameborder="0"></iframe>
+    </div>
   </div>
 </template>
