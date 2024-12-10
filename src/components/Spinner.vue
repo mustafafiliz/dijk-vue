7ec3aad74c2a5040dc9157fc8ff1cda40c59c366
Mustafa <79022605+mustafafiliz@users.noreply.github.com>
2024-12-10 19:53:06 +0300

    fix: approval process item

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="flex justify-center items-center">
-    <div class="animate-spin rounded-full border-4 border-white border-t-transparent h-8 w-8"></div>
+    <div
+      class="animate-spin rounded-full border-4 border-t-transparent"
+      :class="secondary ? 'border-gray-600 h-6 w-6' : 'border-white h-8 w-8'"
+    ></div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    secondary: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
