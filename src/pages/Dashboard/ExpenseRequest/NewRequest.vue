9190e2ac74f136b3667edf1fa462ec0c3340447a
Mustafa <79022605+mustafafiliz@users.noreply.github.com>
2024-12-21 11:53:43 +0300

    feat:  expences delete and edit functionality

--- src/pages/Dashboard/ExpenseRequest/NewRequest.vue
+++ src/pages/Dashboard/ExpenseRequest/NewRequest.vue
@@ -94,6 +94,10 @@ const openModal = (index = null) => {
   showModal.value = true
 }
 
+const deleteExpenseLine = (index) => {
+  expenseLines.value.splice(index, 1)
+}
+
 const handleFileUpload = (event) => {
   const file = event.target.files[0]
   if (file) {
@@ -242,9 +246,52 @@ onMounted(() => {
                 <p>Tarih: {{ line.receipt_date.toISOString().split('T')[0] }}</p>
               </div>
 
-              <div v-if="line.document" class="text-xs text-blue-600 flex items-center gap-1">
+              <div
+                v-if="line.document"
+                class="text-xs text-blue-600 flex items-center justify-between gap-1"
+              >
                 <div>
                   <span class="text-gray-600 text-xs">Dosya:</span> {{ line.document.name }}
+                </div>
+                <div class="flex items-center gap-2">
+                  <button
+                    class="text-blue-600 text-xs flex items-center gap-1"
+                    @click.stop="openModal(index)"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+                      <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+                    </svg>
+                    Düzenle
+                  </button>
+
+                  <button
+                    class="text-red-600 text-xs flex items-center gap-1"
+                    @click.stop="deleteExpenseLine(index)"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path d="M3 6h18" />
+                      <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
+                      <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+                    </svg>
+                    Sil
+                  </button>
                 </div>
               </div>
             </div>
